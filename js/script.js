095a8ccc0e675ff61ba9b02d704f2f2362b48e0f
Nebiyu Daniel <nebiyu.daniel@stu.ocsb.ca>
2025-04-01 14:08:44 +0000

    area of a triangle

--- js/script.js
+++ js/script.js
@@ -1,5 +1,22 @@
-// Copyright (c) 2020 Mr. Coxall All rights reserved
+// Copyright (c) 2022 Ms Raffin All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Ms Raffin
+// Created on: March 2022
 // This file contains the JS functions for index.html
+
+"use strict";
+/**
+ * This function calculates area and perimeter of triangle.
+ */
+function calculate () {
+  // input
+  let length = parseInt(document.getElementById('length-of-triangle').value);
+  let width = parseInt(document.getElementById('width-of-triangle').value);
+
+  // process
+  let area = (length * width) / 2;
+
+  // output
+  document.getElementById('area').innerHTML = 'Area is: ' + area + ' cm²'
+
+}
